2b2ceb3ba97b049f655c981ba88b7a91ba97a9fb
Anastasia-Dmv <Vesta50000@gmail.com>
2020-08-17 17:30:06 +0300

    hw3

---
+++ src/components/phonebook/filter/Filter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './filter.module.css';
+
+export default function Filter({value, onChangeFilter}) {
+    return (
+        <div>
+            <input  className={style.input} type="text" value={value} onChange={e=>onChangeFilter(e.target.value)}/>
+        </div>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React , {StrictMode} from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+
+
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root')
+);
+
